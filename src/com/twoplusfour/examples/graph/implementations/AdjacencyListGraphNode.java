c647eb286b8ef18189f6780c5ea7731ae49549fc
Anthony <acorbelli@gmail.com>
2019-04-02 18:49:35 -0600

    Initial Commit
    Basic Adjacency List, Basic Adjacency Matrix, no operations.

---
+++ src/com/twoplusfour/examples/graph/implementations/AdjacencyListGraphNode.java
@@ -0,0 +1,9 @@
+package com.twoplusfour.examples.graph.implementations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdjacencyListGraphNode<T> {
+    public T value;
+    public ArrayList<T> neighbors;
+}
---
+++ src/com/twoplusfour/examples/graph/implementations/AdjacencyMatrixGraph.java
@@ -0,0 +1,8 @@
+package com.twoplusfour.examples.graph.implementations;
+
+
+import java.util.HashMap;
+
+public class AdjacencyMatrixGraph<T> {
+    public HashMap<T, HashMap<T, Boolean>> edges;
+}
